aadc9513129b9c99afd19ca8afd5a69e29eab285
Adam Campbell <acampbellb@hotmail.com>
2023-04-12 21:27:57 -0400

    created depth camera package

---
+++ ros2bot_camera/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+from glob import glob
+from setuptools import setup
+
+package_name = 'ros2bot_camera'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='acampbellb@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
